df82dd6551c0631c1b9222283f37eb8656016d8e
olamiposiakinade2-byte <olamiposiakinade2@icloud.com>
2026-09-04 12:33:11 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const myFavouriteSubject="Mathematics";
+console.log("My favourite subject is " + myFavouriteSubject);
+
+// Ex2
+const colors = ["Red", "Green", "Blue", "Yellow", "Purple",];
+console.log("First color: ", colors[0]);
+console.log('Number of colors: ', colors.length);
+
+// Ex3
+const favouriteMovie = {
+    title: "Inception",
+    director: "Christopher Nolan",
+    year: 2010,
+    rating: 8.8,
+};
+console.log("Favourite movie: " + favouriteMovie.title);
+
+// Ex4
+const student = {
+    Name:"Akinade Olamiposi",
+    grades: [90, 85, 92, 88, 95],
+    gpa: 4.9,
+};
+console.log("First grade: ", student.grades[0]);
